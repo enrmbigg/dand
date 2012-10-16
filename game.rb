cf08048e51443653d01593586e7498f42d4afe55
Michael Biggs <M.Biggs@2009.ljmu.ac.uk>
2012-10-16 15:26:45 +0100

    New files

---
+++ game.rb
@@ -0,0 +1,8 @@
+name = "Aaron"
+health ="60"
+puts "#{name}\'s health is #{health}"
+
+name1 = 'Molvak'
+name2 = 'Syntha'
+
+puts "Players: \t\n #{name}\n #{name1}\n #{name2}"
---
+++ test.rb
@@ -0,0 +1,7 @@
+puts "Welcome!!"
+
+greetings = "Welcome Ruby!!"
+3.times do 
+	puts greetings.upcase
+end
+puts Time.new
